a17862f37cd80c02d123e9add39aecc09e787bb9
lewisp6 <lewisp6@gmail.com>
2018-09-11 19:11:05 +0100

    Update statuses from api on window focus

--- window.js
+++ window.js
@@ -1,10 +1,17 @@
 const status = require('./tfl/status');
 
-document.addEventListener('DOMContentLoaded', async function() {
+document.addEventListener('DOMContentLoaded', function() {
+  fetchTubeStatuses();
+});
+
+window.addEventListener('focus', function() {
+  fetchTubeStatuses();
+});
+
+async function fetchTubeStatuses() {
   const tubeStatuses = await status.getStatuses();
-  console.log('hello');
   tubeStatuses.forEach((tubeLine) => {
     const element = document.getElementById(`${tubeLine.id}`);
     element.innerHTML = tubeLine.lineStatuses[0]['statusSeverityDescription'];
   });
-});
+}
