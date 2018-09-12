c8b3279b735845bae392fbddc272410d6c21e45d
lewisp6 <lewisp6@gmail.com>
2018-09-12 07:34:16 +0100

    Restructure code and style changes

--- window.js
+++ window.js
@@ -2,14 +2,6 @@ const R = require('ramda');
 const status = require('./tfl/status');
 const domWriter = require('./dom/domWriter');
 
-document.addEventListener('DOMContentLoaded', function() {
-  writeStatusesToDom();
-});
-
-window.addEventListener('focus', function() {
-  writeStatusesToDom();
-});
-
 async function setTubeStatuses(tubeStatuses) {
   tubeStatuses.forEach((tubeLine) => {
     domWriter.writeDom(tubeLine.id, tubeLine.lineStatuses[0]['statusSeverityDescription']);
@@ -17,3 +9,11 @@ async function setTubeStatuses(tubeStatuses) {
 }
 
 const writeStatusesToDom = R.composeP(setTubeStatuses, status.getStatuses);
+
+document.addEventListener('DOMContentLoaded', function() {
+  writeStatusesToDom();
+});
+
+window.addEventListener('focus', function() {
+  writeStatusesToDom();
+});
